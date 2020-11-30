be331800590b17299d24f9359ba54ed6dca36655
takuzen <takuzen0430@gmail.com>
2020-11-30 12:13:10 +0900

    added global css and _app.tsx to reflect that

--- front/core/components/Layout.tsx
+++ front/core/components/Layout.tsx
@@ -6,7 +6,7 @@ type Props = {
   title?: string
 }
 
-const Layout = ({ children, title = 'This is the default title' }: Props) => (
+const Layout = ({ children, title = 'Pocky' }: Props) => (
   <div>
     <Head>
       <title>{title}</title>
--- front/core/pages/index.tsx
+++ front/core/pages/index.tsx
@@ -1,15 +1,39 @@
-import Link from 'next/link'
 import Layout from '../components/Layout'
 import styled from 'styled-components'
 
 const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <StoreName>Pocky</StoreName>
-    <StoreGenre>SALON & STORE </StoreGenre>
+  <Layout title="Pocky Home">
+    <Wrapper>
+      <LeftPanel></LeftPanel>
+      <CenterPanel>
+        <StoreInfo>
+          <StoreName>Pocky</StoreName>
+          <StoreGenre>SALON & STORE </StoreGenre>
+        </StoreInfo>
+      </CenterPanel>
+      <RightPanel></RightPanel>
+    </Wrapper>
   </Layout>
 )
 
 export default IndexPage
+
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: grid;
+  grid-template-columns: 10% 60% 30%;
+`
+const LeftPanel = styled.section``
+
+const CenterPanel = styled.section`
+  display: grid;
+  grid-template-rows: 90% 10%;
+`
+
+const StoreInfo = styled.div``
+
+const RightPanel = styled.section``
 
 const StoreName = styled.p``
 
