9d0581f3daf3316838bdfb71f8d0f42de0ed3664
takuzen <takuzen0430@gmail.com>
2020-12-01 18:33:09 +0900

    modified index.tsx Wrapper's style

--- front/core/pages/index.tsx
+++ front/core/pages/index.tsx
@@ -22,19 +22,30 @@ const Wrapper = styled.div`
   width: 100vw;
   height: 100vh;
   display: grid;
-  grid-template-columns: 10% 60% 30%;
+  grid-template-columns: 20% 60% 20%;
 `
 const LeftPanel = styled.section``
 
 const CenterPanel = styled.section`
   display: grid;
   grid-template-rows: 90% 10%;
+  justify-items: center;
+  align-items: center;
 `
 
-const StoreInfo = styled.div``
+const StoreInfo = styled.div`
+  line-height: 2;
+`
 
 const RightPanel = styled.section``
 
-const StoreName = styled.p``
+const StoreName = styled.p`
+  @import url('https://fonts.googleapis.com/css2?family=Sansita+Swashed:wght@700&display=swap');
+  font-family: 'Sansita Swashed', cursive;
+  font-size: 100px;
+  text-align: center;
+`
 
-const StoreGenre = styled.p``
+const StoreGenre = styled.p`
+  text-align: center;
+`
