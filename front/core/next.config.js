141cd3720567fcaf91c6b447235de69c1b888ef5
takuzen <takuzen0430@gmail.com>
2020-12-29 21:55:33 +0900

    changed next.config.js

--- front/core/next.config.js
+++ front/core/next.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   images: {
-    domains: ['www.google.com'],
+    domains: [
+      'https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12964.92161076721!2d139.7088684!3d35.6713284!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0:0xebee27411fb235b0!2sPocky SALON & STORE!5e0!3m2!1sen!2sjp!4v1608901241643!5m2!1sen!2sjp',
+    ],
   },
 }
